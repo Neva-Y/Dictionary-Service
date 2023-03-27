d04bef637b346bb5ac5cc46f998e64d6ad2097f1
Goh Xing Yang <69152768+Neva-Y@users.noreply.github.com>
2023-03-28 01:27:01 +1100

    adding server socket implementation in application

--- modules/application/src/main/java/com/project/application/DictionaryServiceApplication.java
+++ modules/application/src/main/java/com/project/application/DictionaryServiceApplication.java
@@ -2,22 +2,65 @@ package com.project.application;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-@EnableScheduling
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, WebMvcAutoConfiguration.class})
+@SpringBootApplication
 @Configuration
 public class DictionaryServiceApplication {
+    private static final String ip = "localhost";
+    private static final int port = 4887;
+    private static int counter = 0;
+    private static final Logger logger = LoggerFactory.getLogger(DictionaryServiceApplication.class);
+
     public static void main(String[] args) {
-        final Logger logger = LoggerFactory.getLogger(DictionaryServiceApplication.class);
         new SpringApplicationBuilder(DictionaryServiceApplication.class).run(args);
         logger.info("Application is up!");
+
+        try {
+            ServerSocket server = new ServerSocket(port);
+            server.setReuseAddress(true);
+            logger.info("Waiting for client connection-");
+
+            while(true) {
+                Socket client = server.accept();
+                counter++;
+                logger.info("Client "+counter+": Applying for connection!");
+                logger.info("Client port is: " + client.getPort());
+                logger.info("Client local port is: " + client.getLocalPort());
+                logger.info("Server local port is: " + server.getLocalPort());
+//				System.out.println(server.getPort());
+                // Start a new thread for a connection
+                Thread t = new Thread(() -> serveClient(client));
+                t.start();
+            }
+        } catch (IOException e) {
+            logger.error("IO Exception occurred", e);
+        }
+    }
+
+    private static void serveClient(Socket client)
+    {
+        try(Socket clientSocket = client)
+        {
+            // Input stream
+            DataInputStream input = new DataInputStream(clientSocket.getInputStream());
+            // Output Stream
+            DataOutputStream output = new DataOutputStream(clientSocket.getOutputStream());
+
+            System.out.println("CLIENT: "+input.readUTF());
+
+            output.writeUTF("Server: Hi Client "+counter+" !!!");
+        }
+        catch (IOException e)
+        {
+            logger.error("IO Exception occurred", e);
+        }
     }
 }
--- modules/repository/src/main/java/com/project/repository/TestJob.java
+++ modules/repository/src/main/java/com/project/repository/TestJob.java
@@ -3,9 +3,10 @@ package com.project.repository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
+@EnableScheduling
 @Component
 public class TestJob {
 
