8c7aab27f660353072230eb662e35a06dc511f3a
Goh Xing Yang <69152768+Neva-Y@users.noreply.github.com>
2023-04-12 10:50:40 +1000

    Handled server error messages as the enum class instead of string

--- modules/application/src/main/java/com/project/application/common/ServerError.java
+++ modules/application/src/main/java/com/project/application/common/ServerError.java
@@ -15,18 +15,18 @@ public class ServerError {
     }
 
     public static String ErrorResponse(String error) {
-        switch (error) {
-            case "WORD_NOT_PROVIDED":
+        switch (ErrorCodes.valueOf(error)) {
+            case WORD_NOT_PROVIDED:
                 return "No word specified to the operation";
-            case "WORD_NOT_FOUND":
+            case WORD_NOT_FOUND:
                 return "Given word does not exist in the dictionary";
-            case "INVALID_WORD_FORMAT":
+            case INVALID_WORD_FORMAT:
                 return "Given word is in an invalid format";
-            case "MEANING_NOT_PROVIDED":
+            case MEANING_NOT_PROVIDED:
                 return "No meaning provided for the given word";
-            case "WORD_ALREADY_EXISTS":
+            case WORD_ALREADY_EXISTS:
                 return "Given word already exists in the dictionary";
-            case "UNEXPECTED_OPERATION":
+            case UNEXPECTED_OPERATION:
                 return "Unexpected operation provided";
             default:
                 return "Unexpected error code provided";
